2546bea244a73ed3646fdf1db6ce5da5e7714ece
Ega Arya <ega0110@gmail.com>
2019-03-18 10:16:30 +0700

    Notification

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     // API
-    var _url = 'http://my-json-server.typicode.com/hallowauror/latihan_pwa_api/Mahasiswa';
+    var _url = 'https://my-json-server.typicode.com/hallowauror/latihan_pwa_api/Mahasiswa';
+    // var _url = 'http://localhost/b2/index.php';
 
     // menampung data yang didapat dari API
     var result = '';
@@ -11,7 +12,8 @@ $(document).ready(function () {
     // menampung semua gender dari API
     var gender = [];
 
-    $.get(_url,function (data) {
+    //  $.get(_url,function (data) {
+        function renderPage (data){
         $.each(data, function (key, items) {
             // untuk menampung gender sementara pd loop
             _gend = items.gender;
@@ -40,6 +42,99 @@ $(document).ready(function () {
         // menggunakan selector ID gender-select,
         // kemudian replace html di dalam komponen yang
         // ada di id gender-select menjadi gender_opt
-        $('#gender-select').html(gender_opt);
+        $('#gender-select').html('<option value="semua">semua</option>'+gender_opt);
+    //});
+    }
+
+    var networkDataReceive = false;
+    /* cek di Cache apakah sudah ada? jika belum maka ambil data dari service online */
+    var networkUpdate = fetch(_url).then(function(response){
+        return response.json();
+    }).then(function(data){
+        networkDataReceive = true;
+        renderPage(data)
+    });
+
+    /* Fetch data dari cache */
+    caches.match(_url).then (function(response){
+        if(!response) throw Error ("no data on cache");
+        return response.json();
+    }).then (function(data){
+        if(!networkDataReceive){
+            renderPage(data);
+            console.log('render data from cache');
+        }
+    }).catch(function() {
+        return networkUpdate;
     })
+
+    // filter untuk option gender
+    $('#gender-select').on('change', function () {
+        updateList($(this).val());
+    });
+
+    function updateList(opt) {
+        var _url2 = _url;
+
+        if (opt !== 'semua'){
+            _url2 = _url + '?gender='+opt;
+        }
+        // menampung data yang didapat dari API
+        var result = '';
+
+        $.get(_url2,function (data) {
+            $.each(data, function (key, items) {
+                // untuk menampung gender sementara pd loop
+                _gend = items.gender;
+
+                // untuk memasukkan data ke result dari API
+                result += '<div>'+'<p><b>'+items.name+'</b></p>'+
+                    '<p>'+_gend+'</p></div>';
+            });
+
+            // update list
+            $('#mhs-list').html(result);
+        });
+    }
+
+    Notification.requestPermission(function(status){
+        console.log('Notification permission status : ', status);
+    });
+
+    function displayNotification(){
+        if (Notification.permission === 'granted'){
+            navigator.serviceWorker.getRegistration()
+            .then(function(reg){
+                var options = {
+                    body : 'Ini body notifikasi',
+                    icon : 'images/logo.png',
+                    vibrate : [100,50,10],
+                    data : {
+                        dateOfArrival : Date.now(),
+                        primaryKey : 1
+                    },
+                    actions : [
+                        {action : 'explore', title : 'Kunjungi Situs'},
+                        {action : 'close', title : 'Tutup'}
+                    ]
+                }
+                reg.showNotification('Judul Notifikasi', options);
+            })
+        }
+    }
+    $('#btn-notification').on('click', function(){
+        displayNotification();
+    });
+
 });
+
+
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/serviceworker.js').then(function (reg) {
+            console.log('SW regis sukses dgn skop',reg.scope)
+        }, function (err) {
+            console.log('SW regis failed',err);
+        })
+    })
+} 
